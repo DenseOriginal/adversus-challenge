5fa213077ebdad8686a369d2d3130387a184824a
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-05-06 10:23:52 +0200

    Added recent sales table

--- _frontend/src/ts/components/views/recent-sales.tsx
+++ _frontend/src/ts/components/views/recent-sales.tsx
@@ -1,10 +1,62 @@
 import React from "react";
+import { BackendService, Product, User } from "../../services/backend";
 
-export class RecentSalesView extends React.Component<{}> {
-	
+interface ExtendedSalesEvent {
+	type: 'sale';
+	user: User;
+	product: Product;
+	duration: number;
+}
+interface State {
+	sales: Array<ExtendedSalesEvent>;
+}
+
+export class RecentSalesView extends React.Component<{}, State> {
+	state: State = {
+		sales: [],
+	}
+
+	backendService = BackendService.Instance;
+
+	componentDidMount() {
+		this.backendService.registerSalesEventListener(async (event) => {
+			const { userId, productId, duration } = event;
+
+			// Map the sales event to the correct format
+			const extendedSaleEvent: ExtendedSalesEvent = {
+				type: 'sale',
+				user: await this.backendService.getUser(userId),
+				product: await this.backendService.getProduct(productId),
+				duration,
+			}
+
+			// Update the state
+			this.setState((state) => ({ sales: [extendedSaleEvent, ...state.sales].slice(0, 10) }));
+		});
+	}
+
 	render() {
 		return (
-			''
+			<table className="table recent">
+				<thead>
+					<tr>
+						<th>User</th>
+						<th>Product</th>
+						<th>Price</th>
+						<th>Duration</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.sales.map(sale => 
+						<tr>
+							<td>{sale.user.name}</td>
+							<td>{sale.product.name}</td>
+							<td>{sale.product.unitPrice}</td>
+							<td>{sale.duration}</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
 		)
 	}
 }
