df9332e74afba3c29aa104d084fc983161a63369
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-05-06 19:06:15 +0200

    Added splash component

--- _frontend/src/ts/components/index.tsx
+++ _frontend/src/ts/components/index.tsx
@@ -7,20 +7,27 @@ import { Header } from './views/header';
 
 import { SalesEventHub  } from '../services/messages';
 import { EntityStore } from '../services/meta-store';
+import { BackendService, Product, SalesEvent, User } from '../services/backend';
 
 interface State {
 	mode: 'top' | 'recent';
 	splash: boolean;
+	mostRecentSale: { user: User; product: Product };
 	// ...
 }
+
 export class DashboardApplication extends React.Component<{}, State> {
 	state: State = {
 		mode: 'top',
-		splash: true
+		splash: false,
+		mostRecentSale : undefined,
 	}
 
+	backendService = BackendService.Instance;
+
 	componentDidMount(): void {
-		setTimeout(this.switchMode.bind(this), this.state.mode === 'top' ? 5000 : 5000);
+		setTimeout(this.switchMode.bind(this), this.state.mode === 'top' ? 60000 : 30000);
+		this.backendService.registerSalesEventListener(this.handleSalesEvent.bind(this));
 	}
 
 	render() {
@@ -37,7 +44,7 @@ export class DashboardApplication extends React.Component<{}, State> {
 							: <TopSalesView />
 					}
 
-					{splash && <SplashModal />}
+					{splash && <SplashModal event={this.state.mostRecentSale} />}
 				</div>
 			</>
 		)
@@ -45,6 +52,39 @@ export class DashboardApplication extends React.Component<{}, State> {
 
 	switchMode() {
 		this.setState({ mode: this.state.mode === 'recent' ? 'top' : 'recent' })
-		setTimeout(this.switchMode.bind(this), this.state.mode === 'top' ? 5000 : 5000);
+		setTimeout(this.switchMode.bind(this), this.state.mode === 'top' ? 60000 : 30000);
+	}
+
+	splashQueue: SalesEvent[] = [];
+	handleSalesEvent(event: SalesEvent) {
+		this.splashQueue.push(event);
+		console.log('New sale event');
+		
+		// If there isn't already a splash up, then we need to kickstart the splash timer
+		// Otherwise if it's already going, it will automaticly show the next splash message
+		if (!this.state.splash) this.displaySplash.bind(this)();
+	}
+
+	async displaySplash() {
+		console.log('Display called');
+		
+		const mostRecentSale = this.splashQueue.shift();
+
+		if (!mostRecentSale) {
+			return this.setState({
+				splash: false,
+				mostRecentSale: undefined,
+			});
+		}
+
+		this.setState({
+			splash: true,
+			mostRecentSale: {
+				user: await this.backendService.getUser(mostRecentSale.userId),
+				product: await this.backendService.getProduct(mostRecentSale.productId),
+			},
+		});
+
+		setTimeout(this.displaySplash.bind(this), 5000);
 	}
 }
--- _frontend/src/ts/components/widgets/splash-modal.tsx
+++ _frontend/src/ts/components/widgets/splash-modal.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import Table from 'react-table'
+import { BackendService, Product, SalesEvent, User } from "../../services/backend";
 
-export class SplashModal extends React.Component<{}> {
-	
+interface Props {
+	event: { user: User; product: Product };
+}
+
+export class SplashModal extends React.Component<Props> {
+	backendService = BackendService.Instance;
+
 	render() {
 		return (
-			''
+			// Need to add a key to make react rerender the splash, and therefore trigger the timeout animation
+			<div className="splash" key={Date.now()}>
+				<p>{this.props.event.user.name} sold {this.props.event.product.name}</p>
+				<span className="timeout"></span>
+			</div>
 		)
 	}
 }
